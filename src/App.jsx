240c4b1c247bb470ad14bbc3e639e478ced784d1
nipaaa <akternipa941@gmail.com>
2023-10-28 17:50:49 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import CenterdSlider from "./components/CenterdSlider";
 import MultipleRows from "./components/MultipleRows";
+import Multislider from "./components/Multislider";
 
 function App() {
   return (
     <div>
       <CenterdSlider />
       <MultipleRows />
+      <Multislider/>
     </div>
   );
 }
