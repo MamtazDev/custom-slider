5e81ddc0ebab9719b6be182bf4d64d344dc11f74
nipaaa <akternipa941@gmail.com>
2023-10-28 17:09:15 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import CenterdSlider from "./components/CenterdSlider";
+import MultipleRows from "./components/MultipleRows";
+
+function App() {
+  return (
+    <div>
+      <CenterdSlider />
+      <MultipleRows />
+    </div>
+  );
+}
+
+export default App;
